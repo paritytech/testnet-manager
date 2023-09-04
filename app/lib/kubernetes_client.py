bae6f4f3d7ba56845cb6bfae3c352e839e780f10
Anthony Lazam <lazam@users.noreply.github.com>
2023-09-04 09:46:54 +0200

    Add kubernetes metadata for each node (#99)
    * Add kubernetes metadata for each node

--- app/lib/kubernetes_client.py
+++ app/lib/kubernetes_client.py
@@ -5,6 +5,108 @@ from app.config.network_configuration import get_namespace, network_external_val
 namespace = get_namespace()
 
 
+def get_pod(pod_name):
+    pod = kubernetes_client.CoreV1Api().read_namespaced_pod(namespace=namespace, name=pod_name)
+    return pod
+
+
+def get_pod_details(pod_name):
+    pod = get_pod(pod_name)
+    volume_details = []
+
+    # Get the specs of each volume
+    for volume in pod.spec.volumes:
+        if volume.persistent_volume_claim:
+            pvc_name = volume.persistent_volume_claim.claim_name
+            volume_specs = get_pod_pvc_details(namespace, pvc_name)
+            volume_details.append({
+                'pvc_name': pvc_name,
+                'size': volume_specs['size'],
+                'class': volume_specs['class'],
+                'phase': volume_specs['phase']
+            })
+
+
+    affinity = pod.spec.affinity
+    if affinity:
+        affinity_details = get_affinity_details(affinity)
+    else:
+        affinity_details = {}
+
+    # Get the node name where the pod is scheduled
+    pod_node_location = pod.spec.node_name
+    node_details = get_pod_node_details(pod_node_location)
+
+    potential_ready = pod.status.phase == "Running"
+    container_resources = pod.spec.containers[0].resources
+
+    pod_details = {
+        'affinity': affinity_details,
+        'limit_cpu': container_resources.limits.get('cpu', '') if container_resources.limits else 'None',
+        'limit_memory': container_resources.limits.get('memory', '') if container_resources.limits else 'None',
+        'name': pod.metadata.name,
+        'namespace': pod.metadata.namespace,
+        'node_details': node_details,
+        'node_name': pod.spec.node_name,
+        'node_selector': pod.spec.node_selector or {},
+        'potential_ready': potential_ready,
+        'request_cpu': container_resources.requests.get('cpu', '') if container_resources.requests else 'None',
+        'request_memory': container_resources.requests.get('memory', '') if container_resources.requests else 'None',
+        'restart_count': pod.status.container_statuses[0].restart_count,
+        'tolerations': pod.spec.tolerations or [],
+        'volume_details': volume_details
+    }
+    return pod_details
+
+
+def get_pod_pvc_details(namespace, pvc_name):
+    pvc = kubernetes_client.CoreV1Api().read_namespaced_persistent_volume_claim(namespace=namespace, name=pvc_name)
+    pvc_details = {
+        'size': pvc.spec.resources.requests['storage'],
+        'class': pvc.spec.storage_class_name,
+        'phase': pvc.status.phase
+    }
+    return pvc_details
+
+
+def get_pod_node_details(pod_name):
+    node = kubernetes_client.CoreV1Api().read_node(name=pod_name)
+    node_details = {}
+
+    for label in ["kubernetes.io/arch", "node.kubernetes.io/instance-type", "kubernetes.io/os"]:
+        if label in node.metadata.labels:
+            node_details[label] = node.metadata.labels[label]
+
+    if node.status.node_info.kernel_version:
+        node_details['kernel-version'] = node.status.node_info.kernel_version
+        
+    return node_details
+
+
+def get_affinity_details(affinity):
+    affinity_types = [("node_affinity", affinity.node_affinity),
+                      ("pod_affinity", affinity.pod_affinity),
+                      ("pod_anti_affinity", affinity.pod_anti_affinity)]
+    affinity_details = {}
+
+    for affinity_type, affinity_obj in affinity_types:
+        if affinity_obj:
+            required_during_scheduling = affinity_obj.required_during_scheduling_ignored_during_execution
+            if required_during_scheduling:
+                affinity_details[affinity_type] = {
+                    "policy": "required_during_scheduling",
+                    "details": required_during_scheduling
+                }
+
+            preferred_during_scheduling = affinity_obj.preferred_during_scheduling_ignored_during_execution
+            if preferred_during_scheduling:
+                affinity_details[affinity_type] = {
+                    "policy": "preferred_during_scheduling",
+                    "details": preferred_during_scheduling
+                }
+    return affinity_details
+
+
 def list_stateful_sets():
     return kubernetes_client.CustomObjectsApi().list_namespaced_custom_object(group="apps", version="v1",
                                                                               plural="statefulsets",
@@ -26,11 +128,6 @@ def list_parachain_collator_stateful_sets(para_id):
                            sts['spec']['template']['metadata']['labels'].get('paraId') == para_id,
                stateful_sets['items']))
     return list(map(lambda sts: sts['metadata']['name'], collator_stateful_sets))
-
-
-def get_pod(pod_name):
-    pod = kubernetes_client.CoreV1Api().read_namespaced_pod(namespace=namespace, name=pod_name)
-    return pod
 
 
 def list_substrate_node_pods(role_label=''):
